6b0e2cb2885c5e84c61ad5fb17fe75f21a5c51e9
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2021-08-12 22:53:36 +0200

    Initial commit

---
+++ src/main/resources/static/precache-manifest.584083efce024cea61b39c4f82a2e30e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7565c8e0118bc1e42b718d7256057ffa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec10639568125c1097f9",
+    "url": "/static/css/main.ea9681b4.chunk.css"
+  },
+  {
+    "revision": "29db0a126e8bb88ebc59",
+    "url": "/static/js/2.cb15c6b6.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/static/js/2.cb15c6b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec10639568125c1097f9",
+    "url": "/static/js/main.5e260861.chunk.js"
+  },
+  {
+    "revision": "a3dddab36610e8555814",
+    "url": "/static/js/runtime-main.11a42214.js"
+  },
+  {
+    "revision": "2bae0e662d7be41e2a2d25b778154ffa",
+    "url": "/static/media/chevron-down.2bae0e66.svg"
+  },
+  {
+    "revision": "1763899a7748389ac60fcfe8a3060340",
+    "url": "/static/media/login-bg.1763899a.jpg"
+  },
+  {
+    "revision": "1557c27f02cc1ab4309176be97eeefd0",
+    "url": "/static/media/user.1557c27f.svg"
+  }
+]);
